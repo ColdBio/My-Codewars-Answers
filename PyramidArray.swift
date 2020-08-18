bd78b254ab4aef375d1e5269e2cf21906b12a8ee
LunarColony <64978825+LunarColony@users.noreply.github.com>
2020-08-18 18:16:52 +0100

    Create PyramidArray.swift

---
+++ PyramidArray.swift
@@ -0,0 +1,28 @@
+/*
+Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
+pyramid(0) => [ ]
+pyramid(1) => [ [1] ]
+pyramid(2) => [ [1], [1, 1] ]
+pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
+/*
+
+func pyramid(_ n: Int) -> [[Int]] {
+    if n == 0 {
+        return []
+    }
+    
+    let arr = [1]
+
+    if n == 1 {
+        return [[1]]
+    }
+    var result = [[1]]
+
+    for i in 2...n {
+        //for j in 1...n {
+        result += arr.map { Array(repeating: $0, count: i) }
+        //}
+    }
+
+    return result
+}
