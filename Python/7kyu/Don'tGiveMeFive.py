3f248dc8cfc25970a9cbd6d9bf00fe81faa76340
ColdBio <64978825+ColdBio@users.noreply.github.com>
2021-07-10 17:50:44 +0100

    Create Don'tGiveMeFive.py

---
+++ Python/7kyu/Don'tGiveMeFive.py
@@ -0,0 +1,22 @@
+'''
+Don't give me five!
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+'''
+
+def dont_give_me_five(start,end):
+    x = list(range(start, end+1))
+    temp = [str(x) for x in x]
+    new = []
+    print(temp)
+    for each in temp:
+        if each.find('5') == -1:
+            new.append(each)
+            print("does not contain 5")
+    return len(new)
