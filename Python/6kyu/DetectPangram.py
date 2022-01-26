4928fc1cd004ffdcba03a7e52a0aa3e39b70e82d
ColdBio <64978825+ColdBio@users.noreply.github.com>
2022-01-26 19:50:01 +0000

    Create DetectPangram.py

---
+++ Python/6kyu/DetectPangram.py
@@ -0,0 +1,20 @@
+'''
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+'''
+
+
+def is_pangram(s):
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    cleaned_s = []
+
+    for each in s.lower():
+        if each.isalpha():
+            cleaned_s.append(each)
+
+    if len(set(cleaned_s)) == 26:
+        return True
+    else:
+        return False
