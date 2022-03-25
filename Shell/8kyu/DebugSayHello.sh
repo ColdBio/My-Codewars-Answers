7dfd4c4491d229a00212ca70a0064ca269f6cf67
ColdBio <64978825+ColdBio@users.noreply.github.com>
2022-03-25 21:14:09 +0000

    Create DebugSayHello.sh

---
+++ Shell/8kyu/DebugSayHello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+:'
+Debugging sayHello function
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+'
+say_hello(){
+    echo "Hello, $1"
+}
+say_hello "$1"
